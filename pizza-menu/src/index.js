cc7f5980f65b7ffd7816fa08c9cb301b3147fd72
Muhammad <muhdmahmood@gmail.com>
2023-08-02 10:41:08 +0800

    render all pizza choices dynamically and make symantic        markup of choices with list

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -60,7 +60,13 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+
+      {/*<Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
         photoName="pizzas/spinaci.jpg"
@@ -71,19 +77,21 @@ function Menu() {
         ingredients="Tomato, mushrooms,onion,Capsicum"
         price={12}
         photoName="pizzas/funghi.jpg"
-      />
+      />*/}
     </main>
   );
 }
 function Pizza(props) {
   console.log(props);
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
-      <h3>{props.name}</h3>
-      <p>{props.ingredients}</p>
-      <span>{props.price + 3}</span>
-    </div>
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <div>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price + 3}</span>
+      </div>
+    </li>
   );
 }
 
